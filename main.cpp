116d4d55682f16c834f2d9d1288734c730f4d10b
hunter-2e <hunter2e@yahoo.com>
2022-12-24 04:05:12 -0500

    Start of DFS visual representation

--- main.cpp
+++ main.cpp
@@ -6,34 +6,47 @@
 #include <time.h>
 #include <unistd.h>
 #include <thread>
+#include <queue>
 
 void makeGrid(void);
 void displayGrid(void);
+void generateMaze(void);
 
 using namespace std;
 
-enum size {height = 22, width = 22};
+enum size {height = 23, width = 23};
 vector<vector<char>> grid;
 
 class tank{
     private:
         vector<int> position;
-        
+        char tankLetter;
+
         vector<int> RIGHT ={0,1};
         vector<int> LEFT = {0,-1};
         vector<int> DOWN = {1,0};
         vector<int> UP = {-1,0};
 
+        void setStart(vector<int> startingPosition){
+            position = {(startingPosition[0] * 2) + 1, (startingPosition[1] * 2) + 1};
+
+            grid[(startingPosition[0] * 2) + 1][(startingPosition[1] * 2) + 1] = tankLetter;
+        }
 
 
     public:
-        tank(vector<int> startingPosition){
-            position = startingPosition;
+        tank(vector<int> startingPosition, char tankLetter){
+            this->tankLetter = tankLetter;
+            setStart(startingPosition);
         }
 
         vector<int> getPosition(){
             return position;
         }
+
+};
+
+class graphTracer: private tank {
 
 };
 
@@ -54,11 +67,17 @@ int main(void){
     init_pair(3, COLOR_CYAN, COLOR_CYAN);
     //Color of player two
     init_pair(4, COLOR_GREEN, COLOR_GREEN);
+    //Graph tracer color
+    init_pair(5, COLOR_MAGENTA, COLOR_MAGENTA);
 
     //Start of main
     makeGrid();
+    generateMaze();
+
+    //tank playerOne({0,0}, 'H');
+
     displayGrid();
-    tank playerOne({0,0});
+    
 }
 
 void makeGrid(void){
@@ -94,6 +113,15 @@ void displayGrid(void){
                 case ' ':
                     color = 2;
                     break;
+                case 'H':
+                    color = 3;
+                    break;
+                case 'M':
+                    color = 4;
+                    break;
+                case 'O':
+                    color = 5;
+                    break;
             }
             attron(COLOR_PAIR(color));
             mvprintw(i, j, "   ");
@@ -102,4 +130,24 @@ void displayGrid(void){
         mvprintw(i, width, "\n");
     }
     refresh();
+}
+
+void generateMaze(void){
+    //Creating queue for depth first search
+    queue<vector<int>> nodes;
+
+    //List of visited nodes
+    vector<int> visited;
+
+    //Stating position of DFS
+    vector<int> start = {height - 1, width - 1};
+
+    graphTracer(start, 'O');
+
+    //Push starting position onto queue and mark as visited
+    nodes.push(start);
+    visited.push(start);
+
+
+    
 }
